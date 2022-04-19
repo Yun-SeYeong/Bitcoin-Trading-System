ce5bcc7187f5627c2b5264b4d90ff7db1392d2e6
Yun-SeYeong <dbstpdud09@naver.com>
2022-04-19 21:23:48 +0900

    [INIT] init project

---
+++ src/main/kotlin/com/demo/bitcointradingsystem/BitcoinTradingSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.bitcointradingsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BitcoinTradingSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<BitcoinTradingSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/demo/bitcointradingsystem/BitcoinTradingSystemApplicationTests.kt
@@ -0,0 +1,9 @@
+package com.demo.bitcointradingsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BitcoinTradingSystemApplicationTests {
+
+}
