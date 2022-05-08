f811aaab7e024584c1c21490225a210d5fd164e2
Yun-SeYeong <dbstpdud09@naver.com>
2022-05-08 23:40:16 +0900

    [ADD] Collect Scheduler 추가 (매일 0시 0분 일봉 데이터 저장)

---
+++ src/main/kotlin/com/demo/bitcointradingsystem/scheduler/CollectScheduler.kt
@@ -0,0 +1,22 @@
+package com.demo.bitcointradingsystem.scheduler
+
+import com.demo.bitcointradingsystem.repository.MarketCodeRepository
+import com.demo.bitcointradingsystem.sync.SyncService
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+class CollectScheduler(
+        private val syncService: SyncService,
+        private val marketCodeRepository: MarketCodeRepository
+) {
+
+    @Scheduled(cron = "0 0 0 * * *")
+    fun collectDayCandle() {
+        val marketCodeList = marketCodeRepository.findAll()
+
+        marketCodeList.forEach {
+            syncService.syncDayCandle(it.market, 1)
+        }
+    }
+}
