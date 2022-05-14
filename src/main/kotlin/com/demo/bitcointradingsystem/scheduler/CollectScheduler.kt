bbbe277a8097e6fb014035b58a2a82cb88781f70
Yun-SeYeong <dbstpdud09@naver.com>
2022-05-14 22:03:25 +0900

    [ADD] 3분봉 수집 Scheduler 추가

--- src/main/kotlin/com/demo/bitcointradingsystem/scheduler/CollectScheduler.kt
+++ src/main/kotlin/com/demo/bitcointradingsystem/scheduler/CollectScheduler.kt
@@ -2,10 +2,12 @@ package com.demo.bitcointradingsystem.scheduler
 
 import com.demo.bitcointradingsystem.repository.MarketCodeRepository
 import com.demo.bitcointradingsystem.sync.SyncService
+import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
+@EnableScheduling
 class CollectScheduler(
         private val syncService: SyncService,
         private val marketCodeRepository: MarketCodeRepository
@@ -18,5 +20,20 @@ class CollectScheduler(
         marketCodeList.forEach {
             syncService.syncLastDayCandle(it.market)
         }
+    }
+
+    @Scheduled(cron = "0 0/3 * * * *")
+    fun collect3MinuteCandle() {
+        println("[SYNC] 3분봉 수집 시작")
+        val marketCodeList = marketCodeRepository.findAll()
+
+        marketCodeList.forEach {
+            println("[" + it.market + "] sync...")
+            syncService.syncMinuteCandle(3, it.market, 2)
+            Thread.sleep(200);
+            println("[" + it.market + "] sync complete!")
+        }
+
+        println("[SYNC] 3분봉 수집 완료")
     }
 }
