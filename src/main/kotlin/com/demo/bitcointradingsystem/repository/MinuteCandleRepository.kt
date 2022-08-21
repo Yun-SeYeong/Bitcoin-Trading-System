7ff9c443ba66bb0f87240857dbdc47e991cc38f4
Yun-SeYeong <dbstpdud09@naver.com>
2022-08-21 18:15:10 +0900

    [FIX] delete @Repository annotation

--- src/main/kotlin/com/demo/bitcointradingsystem/repository/MinuteCandleRepository.kt
+++ src/main/kotlin/com/demo/bitcointradingsystem/repository/MinuteCandleRepository.kt
@@ -5,7 +5,6 @@ import com.demo.bitcointradingsystem.entity.MinuteCandleKey
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
-@Repository
 interface MinuteCandleRepository : JpaRepository<MinuteCandle, MinuteCandleKey>{
 
     fun findByMarketAndUnit(market: String, unit: Int): List<MinuteCandle>
