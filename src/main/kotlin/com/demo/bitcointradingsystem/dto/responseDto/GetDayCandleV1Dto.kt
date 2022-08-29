465a7cef464a3a0255976bb592b7535fd143a1bd
Yun-SeYeong <dbstpdud09@naver.com>
2022-08-29 20:59:54 +0900

    [HOTFIX] 일부 CamelCase JsonNaming 적용

--- src/main/kotlin/com/demo/bitcointradingsystem/dto/responseDto/GetDayCandleV1Dto.kt
+++ src/main/kotlin/com/demo/bitcointradingsystem/dto/responseDto/GetDayCandleV1Dto.kt
@@ -1,7 +1,10 @@
 package com.demo.bitcointradingsystem.dto.responseDto
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
 import java.time.LocalDateTime
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class GetDayCandleV1Dto (
         val market: String,
         val candleDateTimeKst: LocalDateTime,
