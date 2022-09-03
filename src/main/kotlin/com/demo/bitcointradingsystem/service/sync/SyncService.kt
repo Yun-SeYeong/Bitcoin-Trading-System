8e0deeb8c4b33b0927843c7704db5fb3bdc86ec1
Yun-SeYeong <dbstpdud09@naver.com>
2022-09-03 16:23:26 +0900

    [FIX] minute candle, day candle bulk insert 최적화

--- src/main/kotlin/com/demo/bitcointradingsystem/service/sync/SyncService.kt
+++ src/main/kotlin/com/demo/bitcointradingsystem/service/sync/SyncService.kt
@@ -7,8 +7,10 @@ import com.demo.bitcointradingsystem.entity.MinuteCandle
 interface SyncService {
     fun syncMinuteCandle(unit: Int, market: String, count: Int) : List<MinuteCandle>
     fun syncMinuteCandleWithDate(unit: Int, market: String, count: Int, to: String): List<MinuteCandle>
+    fun batchMinuteCandleWithDate(unit: Int, market: String, count: Int, to: String): Int
     fun syncDayCandle(market: String, count: Int): List<DayCandle>
     fun syncDayCandleWithDate(market: String, count: Int, to: String): List<DayCandle>
+    fun batchDayCandleWithDate(market: String, count: Int, to: String): Int
     fun syncLastDayCandle(market: String): DayCandle
     fun syncMarketCode(isDetails: Boolean): List<MarketCode>
 }
--- src/main/kotlin/com/demo/bitcointradingsystem/service/sync/SyncServiceImpl.kt
+++ src/main/kotlin/com/demo/bitcointradingsystem/service/sync/SyncServiceImpl.kt
@@ -8,7 +8,10 @@ import com.demo.bitcointradingsystem.repository.MarketCodeRepository
 import com.demo.bitcointradingsystem.repository.MinuteCandleRepository
 import com.demo.bitcointradingsystem.service.joblog.LogService
 import com.demo.bitcointradingsystem.service.upbit.UpbitService
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.sql.Timestamp
 
 @Service
 class SyncServiceImpl(
@@ -16,8 +19,58 @@ class SyncServiceImpl(
         private val minuteCandleRepository: MinuteCandleRepository,
         private val dayCandleRepository: DayCandleRepository,
         private val marketCodeRepository: MarketCodeRepository,
-        private val logService: LogService
+        private val logService: LogService,
+        private val jdbcTemplate: JdbcTemplate
 ) : SyncService {
+
+    @Transactional
+    fun insertMinuteCandle(batchSize: Int, subItems: List<MinuteCandle>): Int {
+        return jdbcTemplate.batchUpdate("insert into minute_candle " +
+                "(candle_acc_trade_price, candle_acc_trade_volume, candle_date_time_kst, candle_date_time_utc, high_price, low_price, opening_price, trade_price, unit, market, timestamp) " +
+                "values " +
+                "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", subItems, batchSize) { ps, argument ->
+            ps.run {
+                setDouble(1, argument.candleAccTradePrice)
+                setDouble(2, argument.candleAccTradeVolume)
+                setTimestamp(3, Timestamp.valueOf(argument.candleDateTimeKst))
+                setTimestamp(4, Timestamp.valueOf(argument.candleDateTimeUtc))
+                setDouble(5, argument.highPrice)
+                setDouble(6, argument.lowPrice)
+                setDouble(7, argument.openingPrice)
+                setDouble(8, argument.tradePrice)
+                setInt(9, argument.unit)
+                setString(10, argument.market)
+                setLong(11, argument.timestamp)
+            }
+        }.sumOf { it.size }
+    }
+
+    @Transactional
+    fun insertDayCandle(batchSize: Int, subItems: List<DayCandle>): Int {
+        return jdbcTemplate.batchUpdate("insert " +
+                "into " +
+                "day_candle " +
+                "(candle_acc_trade_price, candle_acc_trade_volume, candle_date_time_kst, candle_date_time_utc, change_price, change_rate, converted_trade_price, high_price, low_price, opening_price, prev_closing_price, trade_price, market, timestamp) " +
+                "values " +
+                "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", subItems, batchSize) { ps, argument ->
+            ps.run {
+                setDouble(1, argument.candleAccTradePrice)
+                setDouble(2, argument.candleAccTradeVolume)
+                setTimestamp(3, Timestamp.valueOf(argument.candleDateTimeKst))
+                setTimestamp(4, Timestamp.valueOf(argument.candleDateTimeUtc))
+                setDouble(5, argument.changePrice)
+                setDouble(6, argument.changeRate)
+                setDouble(7, argument.convertedTradePrice)
+                setDouble(8, argument.highPrice)
+                setDouble(9, argument.lowPrice)
+                setDouble(10, argument.openingPrice)
+                setDouble(11, argument.prevClosingPrice)
+                setDouble(12, argument.tradePrice)
+                setString(13, argument.market)
+                setLong(14, argument.timestamp)
+            }
+        }.sumOf { it.size }
+    }
 
     override fun syncMinuteCandle(unit: Int, market: String, count: Int): List<MinuteCandle> {
         val createLog = logService.createLog("[SYNC SERVICE] start minute candle sync (unit = [${unit}], market = [${market}], count = [${count}])")
@@ -50,6 +103,22 @@ class SyncServiceImpl(
         return saveAll
     }
 
+    override fun batchMinuteCandleWithDate(unit: Int, market: String, count: Int, to: String): Int {
+        val createLog = logService.createLog("[SYNC SERVICE] start minute candle sync (unit = [${unit}], market = [${market}], count = [${count}], to = [${to}])")
+        val resultSize: Int
+        try {
+            val minuteCandleArray = upbitService.getCandlesMinutes(unit, market, count, to)
+                    ?: throw Exception("Fail to load upbit data")
+            minuteCandleRepository.deleteAllInBatch(minuteCandleArray)
+            resultSize = insertMinuteCandle(1000, minuteCandleArray)
+        } catch (e: Exception) {
+            logService.failLog(createLog.id!!, "[SYNC SERVICE] minute candle sync fail ($e)")
+            throw e
+        }
+        logService.successLog(createLog.id!!, "[SYNC SERVICE] minute candle sync success")
+        return resultSize
+    }
+
     override fun syncDayCandle(market: String, count: Int): List<DayCandle> {
         val createLog = logService.createLog("[SYNC SERVICE] start day candle sync (market = [${market}], count = [${count}])")
         val saveAll: List<DayCandle>
@@ -80,6 +149,22 @@ class SyncServiceImpl(
         return saveAll
     }
 
+    override fun batchDayCandleWithDate(market: String, count: Int, to: String): Int {
+        val createLog = logService.createLog("[SYNC SERVICE] start day candle sync (market = [${market}], count = [${count}], to = [${to}])")
+        val resultSize: Int
+        try {
+            val candlesDays = upbitService.getCandlesDays(market, count, to)
+                    ?: throw Exception("Fail to load upbit data")
+            dayCandleRepository.deleteAllInBatch(candlesDays)
+            resultSize = insertDayCandle(1000, candlesDays)
+        } catch (e: Exception) {
+            logService.failLog(createLog.id!!, "[SYNC SERVICE] day candle sync fail ($e)")
+            throw e
+        }
+        logService.successLog(createLog.id!!, "[SYNC SERVICE] day candle sync success")
+        return resultSize
+    }
+
     override fun syncLastDayCandle(market: String): DayCandle {
         val createLog = logService.createLog("[SYNC SERVICE] start last day candle sync (market = [${market}])")
         val result: DayCandle
@@ -99,7 +184,7 @@ class SyncServiceImpl(
         val saveAll: List<MarketCode>
         try {
             val candlesDays = upbitService.getMarketAll(isDetails) ?: throw Exception("Fail to load upbit data")
-            marketCodeRepository.deleteAll()
+            marketCodeRepository.deleteAllInBatch()
             saveAll = marketCodeRepository.saveAll(candlesDays)
         } catch (e: Exception) {
             logService.failLog(createLog.id!!, "[SYNC SERVICE] day candle sync fail ($e)")
